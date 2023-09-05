c1cb1ef4952944a030877da645253bcd0dda254c
gabrielmulleer <emaildegabrielm@gmail.com>
2023-09-05 14:44:22 -0300

    feat: create migrations

---
+++ server/index.js
@@ -0,0 +1,9 @@
+const express = require('express');
+
+const app = express();
+
+const port = 3000;
+
+app.listen(port, () => console.log(`Servidor está rodando na porta ${port}`));
+
+module.exports = app;
