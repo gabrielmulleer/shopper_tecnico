7b5614cc5f023226364925ef2953417f5acbdb29
gabrielmulleer <emaildegabrielm@gmail.com>
2023-09-05 16:20:01 -0300

    feat: add seeders to fill tables

---
+++ server/db/seeders/20230905190758-packs.js
@@ -0,0 +1,38 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.bulkInsert('Packs', [
+      {
+        pack_id: 1000,
+        product_id: 18,
+        qty: 6,
+      },
+      {
+        pack_id: 1010,
+        product_id: 24,
+        qty: 1,
+      },
+      {
+        pack_id: 1010,
+        product_id: 26,
+        qty: 1,
+      },
+      {
+        pack_id: 1020,
+        product_id: 19,
+        qty: 3,
+      },
+      {
+        pack_id: 1020,
+        product_id: 21,
+        qty: 3,
+      },
+    ]);
+  },
+
+  async down(queryInterface) {
+    await queryInterface.bulkDelete('Packs', null, {});
+  },
+};
