f37e6eafda5705c68a89e02854ac1ff0d95c298e
gabrielmulleer <emaildegabrielm@gmail.com>
2023-09-07 17:22:25 -0300

    refactor: more improvements in atualizaProduct route

--- server/controllers/ProductController.js
+++ server/controllers/ProductController.js
@@ -354,7 +354,6 @@ class ProductController {
     }
   }
 }
-
 async function processCSV(buffer) {
   const readableFile = new stream.Readable();
   readableFile.push(buffer);
@@ -367,7 +366,8 @@ async function processCSV(buffer) {
   const updatedProducts = [];
   const skippedProducts = [];
   const nonexistentProducts = [];
-
+  // Objeto para rastrear a soma dos valores dos kits
+  const kitSums = {};
   for await (let line of productsLine) {
     const columns = line.split(',');
     if (!isNaN(Number(columns[0])) && !isNaN(Number(columns[1]))) {
@@ -379,6 +379,109 @@ async function processCSV(buffer) {
         where: { code: code },
       });
 
+      if (product) {
+        const costPrice = parseFloat(product.cost_price);
+        const currentPrice = parseFloat(product.sales_price);
+        const newPriceFloat = parseFloat(newPrice);
+
+        const isValidPrice = validatePrice(
+          currentPrice,
+          newPriceFloat,
+          costPrice
+        );
+        if (isValidPrice) {
+          await updateProductPrice(Product, code, newPriceFloat);
+          updatedProducts.push({
+            code: code,
+            name: product.name,
+            old_price: currentPrice,
+            new_price: newPriceFloat,
+          });
+        } else {
+          skippedProducts.push({
+            code: code,
+            name: product.name,
+            old_price: currentPrice,
+            new_price: getInvalidPriceReason(
+              currentPrice,
+              newPriceFloat,
+              costPrice
+            ),
+          });
+        }
+      } else {
+        nonexistentProducts.push({ code: code });
+      }
+    }
+  }
+
+  return { updatedProducts, skippedProducts, nonexistentProducts };
+}
+
+async function processCSV2(buffer) {
+  const readableFile = new stream.Readable();
+  readableFile.push(buffer);
+  readableFile.push(null);
+
+  const productsLine = readline.createInterface({
+    input: readableFile,
+  });
+
+  const updatedProducts = [];
+  const skippedProducts = [];
+  const nonexistentProducts = [];
+  // Objeto para rastrear a soma dos valores dos kits
+  const kitSums = {};
+  for await (let line of productsLine) {
+    const columns = line.split(',');
+    if (!isNaN(Number(columns[0])) && !isNaN(Number(columns[1]))) {
+      const code = columns[0];
+      const newPrice = columns[1];
+
+      const product = await Product.findOne({
+        attributes: ['code', 'name', 'cost_price', 'sales_price'],
+        where: { code: code },
+      });
+      const count = await Pack.count({ where: { pack_id: code } });
+      const pack = await Pack.findOne({
+        attributes: ['pack_id', 'product_id', 'qty'],
+        where: { [Op.or]: [{ product_id: code }, { pack_id: code }] },
+      });
+
+      if (!kitSums[code]) {
+        // Verifique se o código já existe como pacote no objeto kitSums
+        // e se ele corresponde a um pack na tabela Packs
+        kitSums[code] = Number(newPrice);
+
+        if (pack) {
+          const count = await Pack.count({ where: { pack_id: pack.pack_id } });
+          if (count > 2 && pack.product_id != code) {
+            console.log('teste');
+          }
+          if (count === 1) {
+            kitSums[pack.pack_id] = Number(newPrice) * pack.qty;
+          } else if (count > 1 && !kitSums[pack.pack_id]) {
+            console.log(count);
+            // Se não, crie uma entrada com o valor atual
+            kitSums[pack.pack_id] = Number(newPrice) * pack.qty;
+          } else if (count > 1 && kitSums[pack.pack_id]) {
+            console.log(code);
+            console.log(pack.product_id);
+            console.log(pack.pack_id);
+
+            // Se sim, adicione o valor atual ao valor existente
+            kitSums[pack.pack_id] += Number(newPrice) * pack.qty;
+          }
+        }
+        // Verifica na tabela Packs se o codigo é unico
+        // se for único, ele pegará o valor total do pack e dividira pela quantidade
+        // atualizando o valor do produto e do pack
+        if (count === 1 && pack.pack_id == code) {
+          kitSums[pack.product_id] = Number(newPrice / pack.qty);
+        }
+      }
+
+      console.log(kitSums);
       if (product) {
         const costPrice = parseFloat(product.cost_price);
         const currentPrice = parseFloat(product.sales_price);
